42b2ea1c85eee7f9f873dba01d79e52ad8fa7526
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-08-05 11:09:23 -0400

    Create 1002 - Área del Círculo.cpp

---
+++ 1002 - Área del Círculo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double A,r;
+    cin>>r;
+    A=3.14159*pow(r,2);
+    cout<<fixed<<setprecision(4)<<"A="<<A<<endl;
+}
